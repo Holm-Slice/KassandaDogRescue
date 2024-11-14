ecc0b1c7aece98a8b8d920cce8abfe4b67bc8c28
Holm-Slice <taylorholm92@gmail.com>
2024-11-14 11:48:44 -0600

    changed mobile paypal img back to 250px and set expand prop to false in navbar in hopes of removing hamuber menu

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -4,7 +4,7 @@ import { Navbar } from "react-bootstrap";
 import "./NavBar.css";
 
 const NavBar = () => (
-  <Navbar className="navbar" expand="lg">
+  <Navbar className="navbar" expand="false">
     <Navbar.Brand
       href="#home"
       style={{ fontSize: "45px", textAlign: "center", width: "100%" }}
