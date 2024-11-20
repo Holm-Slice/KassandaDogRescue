e80022128fc65d7363d8b2ee8c2c64ae936399ae
Holm-Slice <taylorholm92@gmail.com>
2024-11-20 15:53:32 -0600

    changed paypal button to new business acct embed

--- src/Components/PayPalEmbed.js
+++ src/Components/PayPalEmbed.js
@@ -1,26 +1,64 @@
 import React from "react";
-import "./PayPalEmbed.css";
-// import paypalImage from "dog-rescue-site/src/images/paypalacct.png"; // Make sure to replace with the actual path to your image
 
 const PayPalEmbed = () => {
-  const handleDonateClick = () => {
-    window.open(
-      "https://paypal.me/animacar70?country.x=US&locale.x=en_US",
-      "_blank",
-      "noopener,noreferrer"
-    );
+  const buttonStyle = {
+    textAlign: "center",
+    border: "none",
+    borderRadius: "1.5rem",
+    minWidth: "11.625rem",
+    padding: "0 2rem",
+    height: "3.125rem",
+    fontWeight: "bold",
+    backgroundColor: "#FFD140",
+    color: "#000000",
+    fontFamily: '"Helvetica Neue", Arial, sans-serif',
+    fontSize: "1.125rem",
+    lineHeight: "1.5rem",
+    cursor: "pointer",
+  };
+
+  const formStyle = {
+    display: "inline-grid",
+    justifyItems: "center",
+    alignContent: "start",
+    gap: "0.5rem",
+  };
+
+  const paypalLogoStyle = {
+    height: "0.875rem",
+    verticalAlign: "middle",
+  };
+
+  const containerStyle = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    // height: "100vh", // Full viewport height
+    // backgroundColor: "#f9f9f9", // Optional background color
   };
 
   return (
-    <div className="paypal-embed">
-      <h2>Support Through PayPal:</h2>
-      <div onClick={handleDonateClick} className="paypal-image-container">
+    <div style={containerStyle}>
+      <form
+        action="https://www.paypal.com/ncp/payment/MTCUVGZRV8XYQ"
+        method="post"
+        target="_top"
+        style={formStyle}
+      >
+        <input style={buttonStyle} type="submit" value="Donate Now" />
         <img
-          src="/imgs/paypalImg.png"
-          alt="Donate with PayPal"
-          className="paypal-image"
+          src="https://www.paypalobjects.com/images/Debit_Credit_APM.svg"
+          alt="cards"
         />
-      </div>
+        <section>
+          Powered by{" "}
+          <img
+            src="https://www.paypalobjects.com/paypal-ui/logos/svg/paypal-wordmark-color.svg"
+            alt="paypal"
+            style={paypalLogoStyle}
+          />
+        </section>
+      </form>
     </div>
   );
 };
